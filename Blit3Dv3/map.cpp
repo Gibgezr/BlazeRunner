d494e5ef5ff617e5adf376a06692727cb53fbfec
Mohammed Al-Safwan <Nyanrex52@gmail.com>
2017-11-04 04:54:04 -0300

    load and save are done

--- Blit3Dv3/map.cpp
+++ Blit3Dv3/map.cpp
@@ -45,7 +45,7 @@ void TileMap::Update(float seconds)
 	}
 }
 
-bool TileMap::SpreadFire(std::vector<Vent>)
+bool TileMap::SpreadFire(std::vector<Vent> vent)
 {
 
 	bool touchedPlayer = false;
@@ -96,8 +96,48 @@ bool TileMap::LoadLevel(std::string filename)
 	{
 		while (getline(mapFile, row))
 		{
-			
-			std::cout << row << '\n';
+			for (int y = 0; y < MAPHEIGHT; ++y)
+			{
+				for (int x = 0; x < MAPWIDTH; ++x)
+				{
+					//TileType{ BASE, SPACE, FLOOR, WALL, DOORH, DOORV, VENT, EXIT };
+					switch (row.c_str()[x]) {
+					case '0':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::SPACE;
+						break;
+					case '1':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::FLOOR;
+						break;
+					case '2':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::WALL;
+						break;
+					case '3':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::DOORH;
+						break;
+					case '4':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::DOORV;
+						break;
+					case '5':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::VENT;
+						break;
+					case '6':
+						theMap[x][y] = new baseTile();
+						theMap[x][y]->tileID = TileType::EXIT;
+						break;
+					default:
+
+						break;
+					}
+
+				}
+			}
+
 		}
 		mapFile.close();
 	}
@@ -112,8 +152,41 @@ void TileMap::SaveLevel(std::string filename)
 	std::ofstream mapFile(filename);
 	if (mapFile.is_open())
 	{
-		mapFile << "This is a line.\n";
-		mapFile << "This is another line.\n";
+
+		for (int y = 0; y < MAPHEIGHT; ++y)
+		{
+			for (int x = 0; x < MAPWIDTH; ++x)
+			{
+				//TileType{ BASE, SPACE, FLOOR, WALL, DOORH, DOORV, VENT, EXIT };
+				switch (theMap[x][y]->tileID) {
+				case TileType::SPACE:
+					mapFile << '0';
+					break;
+				case  TileType::FLOOR:
+					mapFile << '1';
+					break;
+				case  TileType::WALL:
+					mapFile << '2';
+					break;
+				case  TileType::DOORH:
+					mapFile << '3';
+					break;
+				case  TileType::DOORV:
+					mapFile << '4';
+					break;
+				case  TileType::VENT:
+					mapFile << '5';
+					break;
+				case  TileType::EXIT:
+					mapFile << '6';
+					break;
+				default:
+
+					break;
+				}
+			}
+			mapFile << '\n';
+		}
 		mapFile.close();
 	}
 	else std::cout << "Unable to open file";
